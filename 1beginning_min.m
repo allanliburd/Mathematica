01e590cc43018fed914733924b411fa5e141062d
Allan Liburd <allanliburd@zohomail.com>
2020-03-29 00:33:05 -0400

    Commit existing projects on the rpi

---
+++ 1beginning_min.m
@@ -0,0 +1,84 @@
+(* Content-type: application/vnd.wolfram.mathematica *)
+
+(*** Wolfram Notebook File ***)
+(* http://www.wolfram.com/nb *)
+
+(* CreatedBy='Mathematica 12.0' *)
+
+(*CacheID: 234*)
+(* Internal cache information:
+NotebookFileLineBreakTest
+NotebookFileLineBreakTest
+NotebookDataPosition[       158,          7]
+NotebookDataLength[      2384,         76]
+NotebookOptionsPosition[      2095,         63]
+NotebookOutlinePosition[      2431,         78]
+CellTagsIndexPosition[      2388,         75]
+WindowFrame->Normal*)
+
+(* Beginning of Notebook Content *)
+Notebook[{
+Cell[BoxData[{
+ RowBox[{"Print", "[", "\"\<Hello world\>\"", "]"}], "\[IndentingNewLine]", 
+ RowBox[{"2", "+", "2"}], "\[IndentingNewLine]", 
+ RowBox[{"16254", "/", "32"}], "\[IndentingNewLine]", 
+ RowBox[{
+  RowBox[{"1024", "*", "32"}], "\n"}], "\[IndentingNewLine]", 
+ RowBox[{
+  RowBox[{"radius", "=", "5"}], ";"}], "\n", 
+ RowBox[{
+  RowBox[{"diameter", "=", 
+   RowBox[{"2", "*", "radius"}]}], ";"}], "\n", 
+ RowBox[{
+  RowBox[{"circumference", "=", 
+   RowBox[{"2", "*", "Pi", "*", "radius"}]}], ";"}], "\n", 
+ RowBox[{
+  RowBox[{"area", "=", 
+   RowBox[{"Pi", "*", 
+    RowBox[{"radius", "^", "2"}]}]}], 
+  "\[IndentingNewLine]"}], "\[IndentingNewLine]", "Pi", "\[IndentingNewLine]", 
+ RowBox[{
+  RowBox[{"tau", " ", "=", " ", 
+   RowBox[{"2", " ", "*", " ", "Pi"}]}], 
+  "\[IndentingNewLine]"}], "\[IndentingNewLine]", 
+ RowBox[{"N", "[", "Pi", "]"}], "\[IndentingNewLine]", 
+ RowBox[{
+  RowBox[{"N", "[", 
+   RowBox[{"Pi", ",", " ", "10"}], "]"}], 
+  "\[IndentingNewLine]"}], "\[IndentingNewLine]", 
+ RowBox[{"nums", "=", 
+  RowBox[{"{", 
+   RowBox[{"1", ",", "2", ",", "3", ",", "5", ",", "8"}], "}"}]}], "\n", 
+ RowBox[{
+  RowBox[{"people", "=", 
+   RowBox[{"{", 
+    RowBox[{
+    "\"\<Alice\>\"", ",", "\"\<Bob\>\"", ",", "\"\<Charlotte\>\"", ",", 
+     "\"\<David\>\""}], "}"}]}], 
+  "\[IndentingNewLine]"}], "\[IndentingNewLine]", 
+ RowBox[{"Print", "[", "\"\<Bye world\>\"", "]"}]}], "Input",
+ CellChangeTimes->{{3.793662614507163*^9, 
+  3.793662621054446*^9}},ExpressionUUID->"6c483b65-3cf5-445a-b79d-\
+86c814fbedd1"]
+},
+WindowSize->{800, 881},
+WindowMargins->{{172, Automatic}, {-75, Automatic}},
+FrontEndVersion->"12.0 for Linux ARM (32-bit) (June 23, 2019)",
+StyleDefinitions->"Default.nb"
+]
+(* End of Notebook Content *)
+
+(* Internal cache information *)
+(*CellTagsOutline
+CellTagsIndex->{}
+*)
+(*CellTagsIndex
+CellTagsIndex->{}
+*)
+(*NotebookFileOutline
+Notebook[{
+Cell[558, 20, 1533, 41, 469, "Input",ExpressionUUID->"6c483b65-3cf5-445a-b79d-86c814fbedd1"]
+}
+]
+*)
+
